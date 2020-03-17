80ea28e80439e225f64f1cb885f2ef26cd264671
dogchob <32832648+dogchob@users.noreply.github.com>
2020-03-17 15:24:46 +0900

    my first commit

---
+++ database.py
@@ -0,0 +1,29 @@
+import pymysql
+
+
+def get_connection():
+    conn = pymysql.connect(host='127.0.0.1', user='root',
+                           password='1234', db='msg'
+                           , charset='utf8')
+
+    return conn
+
+
+def get_thumbnail_high_views():
+    conn = get_connection()
+    sql = '''select * from thumbnail_tb order by views desc limit 6'''
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    conn.close()
+    return result
+
+
+def get_hashtag(thumbnail_id):
+    conn = get_connection()
+    sql = '''select name from hash_tb where hash_id in (select hash_id from thumbnail_hash where thumbnail_id = %s)'''
+    cursor = conn.cursor()
+    cursor.execute(sql, thumbnail_id)
+    result = cursor.fetchall()
+    conn.close()
+    return result
---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import Flask, render_template, request
+import database as db
+import random
+
+app = Flask(__name__, template_folder='templates')
+app.secret_key = 'MSG'
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+
+@app.route('/')
+def index():
+    views = db.get_thumbnail_high_views()
+    print(views)
+    hashtag = []
+    for i in views:
+        temp = db.get_hashtag(i[0])
+        temp = random.sample(temp, 2)
+        hashtag.append(temp)
+    print(hashtag)
+    html = render_template('index.html', data_list=views, tag_list=hashtag )
+    return html
+
+
+@app.route('/vod', methods=['GET', 'POST'])
+def vod():
+    html = render_template('vod.html')
+    return html
+
+
+@app.route('/info', methods=['GET', 'POST'])
+def info():
+    search = request.args.get('search')
+    html = render_template('info.html', search_arg=search)
+    return html
+
+
+app.run(host='127.0.0.1', port=8080)

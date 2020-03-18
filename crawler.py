62b0a899c76336b74cceef9d903eb5cd19b2cc01
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2020-03-18 17:17:35 +0900

    데이터 수집

--- crawler.py
+++ crawler.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
+from bs4 import BeautifulSoup
 import urllib.request
+import time
 
 driver = webdriver.Chrome('c:/informs/chromedriver.exe')
 
@@ -8,7 +10,25 @@ driver = webdriver.Chrome('c:/informs/chromedriver.exe')
 # urllib.request.urlretrieve(url, 'c:/informs/video/test.mp4')
 
 driver.implicitly_wait(10) # seconds
-driver.get('https://tv.naver.com/v/11789515/list/552637')
-se = driver.find_element_by_xpath('//*[@id="player"]/div/div/div[11]/div[13]/video')
+driver.get('https://tv.naver.com/v/11789515/list/552637') # 사이트 접속
+time.sleep(10)
 
-print(se)
+video = driver.find_element_by_xpath('//*[@id="player"]/div/div/div[11]/div[13]/video')
+title = driver.find_element_by_xpath('//*[@id="clipInfoArea"]/div[1]/h3')
+views = driver.find_element_by_xpath('//*[@id="clipInfoArea"]/div[1]/div/span[1]')
+hash_list = driver.find_elements_by_css_selector('#clipInfoArea > div.hash_box > ul > li')
+
+driver.implicitly_wait(30) # seconds
+time.sleep(2)
+
+video_src = video.get_attribute('src')
+video_title = title.text
+video_views = views.text
+video_hash = []
+
+print(video_src)
+print(video_title)
+print(video_views)
+for h in hash_list:
+    video_hash.append(h.text)
+    print(h.text)

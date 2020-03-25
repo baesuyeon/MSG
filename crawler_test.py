f92383c85c651ff18cc4aa6e0ae1b0aa8247a5f7
baesuyeon <37732016+baesuyeon@users.noreply.github.com>
2020-03-25 15:08:48 +0900

    회차 정보 수정

--- crawler_test.py
+++ crawler_test.py
@@ -2,7 +2,7 @@ import crawler
 
 if __name__ == "__main__" :
     Clips = [] # 클립
-    search_word = '관찰카메라24 119화' # '관찰카메라24 119화', '반의반 1화'
+    search_word = '방법 1화' # '방법 1화', '관찰카메라24 119화', '반의반 1화'
     vod_no = 1
     crawler.crawler(Clips, search_word, vod_no)
 
